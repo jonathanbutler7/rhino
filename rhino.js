d7032ed7b44ac0fc21617b0bb3a1510d4fafcd42
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-05 18:54:36 -0800

    login successful

--- rhino.js
+++ rhino.js
@@ -4,8 +4,13 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.rhino.com/user/login');
+  await page.type('#edit-name', 'rhinotest');
+  await page.type('#edit-pass', process.env.RHINO_PASSWORD);
+
+  await Promise.all([
+    page.waitForNavigation(), // The promise resolves after navigation has finished
+    page.click('#edit-submit'), // Clicking the link will indirectly cause a navigation
+  ]);
   await page.screenshot({ path: 'myscreenshot.png' });
-  await page.type('username', 'rhinotest')
-  await page.type('password', '')
   await browser.close();
 })();
