3bd9e0c132d214ada89769d83a7c3e2776b2e83b
jonathanbutler7 <jonathanbutler7@gmail.com>
2021-01-05 19:17:37 -0800

    removed urls

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-This project is a take home coding challenge for a junior site developer role.
+# What is this project?
+
+**A take home coding challenge for a junior site developer role.**
+
+## The requirements were:
+
+1. Create a new node project and install puppeteer
+2. Create a new file called "rhino.js" to work in
+3. In this file launch a new puppeteer browser and go to: `https://www.rhino.com/user/login`
+4. Login to the site with these credentials: `username` `password`
+5. Once login is complete go to to this page (which you can only view logged in): `testurl`
+6. Grab the text from inside the div with this id:"textToScrape"
+7. Save the text you grabbed to a local text file named: "scraped.txt"
+8. Zip up *just* "rhino.js" and send via email
--- rhino.js
+++ rhino.js
@@ -1,18 +1,19 @@
 const puppeteer = require('puppeteer');
+const { RHINO_LOGIN, RHINO_PASSWORD, RHINO_TESTURL } = process.env;
 const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.rhino.com/user/login');
-  await page.type('#edit-name', 'rhinotest');
-  await page.type('#edit-pass', process.env.RHINO_PASSWORD);
+  await page.type('#edit-name', RHINO_LOGIN);
+  await page.type('#edit-pass', RHINO_PASSWORD);
 
   await Promise.all([
     page.waitForNavigation(), // The promise resolves after navigation has finished
     page.click('#edit-submit'), // Clicking the link will indirectly cause a navigation
   ]);
-  await page.goto('https://www.rhino.com/rhino-test');
+  await page.goto(RHINO_TESTURL);
   const text = await page.evaluate(
     () => document.querySelector('#textToScrape').textContent
   );
